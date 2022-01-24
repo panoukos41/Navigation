8978e465eee0839e07360caea4e9fa8fb97cbc4e
Panos <panos_ath@outlook.com>
2022-01-24 16:53:09 +0200

    Add Exception class

---
+++ src/Navigation/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P41.Navigation.Exceptions;
+
+/// <summary>
+/// Exception thrown when a route is not matched to any mapping.
+/// </summary>
+public class CouldNotNavigateException : Exception
+{
+    /// <summary>
+    /// Initialize a new <see cref="CouldNotNavigateException"/>.
+    /// </summary>
+    public CouldNotNavigateException(string route) : base($"Could not navigate to '{route}'.")
+    {
+    }
+}
